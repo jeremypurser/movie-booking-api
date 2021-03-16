7be448e3316ba2e4475af4f1105d7e7e0c288ca6
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-16 11:16:07 -0400

    Add helpers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,21 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      include Authorization
+      include JwtAuthenticatable
 
       before_action :authorized
+
+      def success_response(object)
+        render json: object
+      end
+
+      def created_response(object)
+        render json: object, status: :created
+      end
+
+      def error_response(payload, status)
+        render json: { data: nil, errors: payload }, status: status
+      end
     end
   end
 end
