ae4e4fe40025b9d74c0c7935674e12e2c53781a3
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-16 17:15:43 -0400

    Cleanup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,40 +1,40 @@
-module Api
-  module V1
-    class UsersController < ApiController
-      before_action :authorize, only: [:test]
+# frozen_string_literal: true
 
-      # Register
-      # POST /api/v1/register
-      def create
-        user = User.create(user_params)
+module Api::V1
+  class UsersController < ApiController
+    before_action :authorize, only: [:test]
 
-        if user.valid?
-          created_response(user)
-        else
-          error_response('Invalid username or password', :unauthorized)
-        end
+    # Register
+    # POST /api/v1/register
+    def create
+      user = User.create(user_params)
+
+      if user.valid?
+        success_response(user, 201)
+      else
+        error_response('Invalid username or password', :unauthorized)
       end
+    end
 
-      # POST /api/v1/login
-      def login
-        user = User.find_by(email: user_params[:email])
+    # POST /api/v1/login
+    def login
+      user = User.find_by(email: user_params[:email])
 
-        if user&.authenticate(user_params[:password])
-          success_response(user)
-        else
-          error_response('Invalid username or password', :unauthorized)
-        end
+      if user&.authenticate(user_params[:password])
+        success_response(user)
+      else
+        error_response('Invalid username or password', :unauthorized)
       end
+    end
 
-      def test
-        render json: { message: 'Success' }
-      end
+    def test
+      render json: { message: 'Success' }
+    end
 
-      private
+    private
 
-      def user_params
-        params.permit(:email, :password, :admin)
-      end
+    def user_params
+      params.permit(:email, :password, :admin)
     end
   end
 end
