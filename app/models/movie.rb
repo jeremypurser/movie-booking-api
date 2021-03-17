80c51c4def10cbe47bacf04bb9433eb14962bc5a
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-17 15:40:12 -0400

    Add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :runtime, presence: true
+  validates :buffer, presence: true
+
+  paginates_per 10
 end
