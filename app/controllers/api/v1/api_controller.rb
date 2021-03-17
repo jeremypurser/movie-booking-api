55f04fc3ca6a53ea932f9947ab75009cd83ebb12
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-17 15:37:10 -0400

    Add pagination method

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,15 +1,23 @@
-module Api
-  module V1
-    class ApiController < ApplicationController
-      include JwtAuthenticatable
+module Api::V1
+  class ApiController < ApplicationController
+    include JwtAuthenticatable
 
-      def success_response(object, status = 200)
-        render json: object, status: status
-      end
+    def success_response(object, status = 200)
+      render json: object, status: status
+    end
 
-      def error_response(payload, status = 400)
-        render json: { data: nil, errors: payload }, status: status
-      end
+    def error_response(payload, status = 400)
+      render json: { data: nil, errors: payload }, status: status
+    end
+
+    def pagination(object)
+      {
+        current_page: object.current_page,
+        next_page: object.next_page,
+        prev_page: object.prev_page,
+        total_pages: object.total_pages,
+        total_count: object.total_count
+      }
     end
   end
 end
