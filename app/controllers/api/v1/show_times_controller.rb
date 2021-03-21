76c936be381891abe818f096924b4c2213d9107d
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-20 20:16:35 -0400

    Add filtering and use ShowTimeProcessor service

--- app/controllers/api/v1/show_times_controller.rb
+++ app/controllers/api/v1/show_times_controller.rb
@@ -4,9 +4,20 @@ module Api::V1
   class ShowTimesController < ApiController
     before_action :admin_access
 
+    # GET /api/v1/showtimes
+    def index
+      show_times = ShowTime.filter(filtering_params).page(params[:page])
+
+      render json: show_times, meta: { pagination: pagination(show_times) }
+    end
+
     # POST /api/v1/showtimes
     def create
-      show_time = ShowTime.new(show_time_params)
+      show_time = ShowTimeProcessor.call(
+        start_time: show_time_params[:start_time],
+        end_time: show_time_params[:end_time],
+        params: show_time_params
+      )
 
       if show_time.save
         success_response(show_time, 201)
@@ -19,6 +30,10 @@ module Api::V1
 
     def show_time_params
       params.permit(:start_time, :end_time, :seat_count, :movie_id)
+    end
+
+    def filtering_params
+      params.slice(:start_time, :end_time, :seat_count, :movie_id)
     end
   end
 end
