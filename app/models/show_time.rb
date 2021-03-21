b87c32bedc463d012a109dcbefb1a100ad8f424d
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-20 20:18:15 -0400

    Add scopes for filtering

--- app/models/show_time.rb
+++ app/models/show_time.rb
@@ -5,4 +5,11 @@ class ShowTime < ApplicationRecord
   validates :end_time, presence: true
   validates :seat_count, presence: true
   validates_with ShowTimeValidator
+
+  scope :filter_by_start_time, ->(start_time) { where start_time: start_time }
+  scope :filter_by_end_time, ->(end_time) { where end_time: end_time }
+  scope :filter_by_seat_count, ->(seat_count) { where seat_count: seat_count }
+  scope :filter_by_movie_id, ->(movie_id) { where movie_id: movie_id }
+
+  paginates_per 10
 end
