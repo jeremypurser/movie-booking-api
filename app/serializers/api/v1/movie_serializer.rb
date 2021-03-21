f792fa90723749f7de294db4023f533226885591
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-20 20:18:59 -0400

    Add show times to movie response

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -2,6 +2,14 @@
 
 module Api::V1
   class MovieSerializer < ApplicationSerializer
-    attributes :id, :name, :runtime, :buffer, :details
+    attributes :id, :name, :runtime, :buffer, :details, :show_times
+
+    def show_times
+      future_show_times = object.show_times.select { |s| s.start_time > Time.now }
+
+      future_show_times.map do |show_time|
+        ShowTimeSerializer.new(show_time)
+      end
+    end
   end
 end
