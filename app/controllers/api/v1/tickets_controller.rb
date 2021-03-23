05df8709f2850aa221aabf560b7ed9083246e457
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-23 17:15:09 -0400

    Refactor create

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -2,13 +2,18 @@
 
 module Api::V1
   class TicketsController < ApiController
+    # POST /api/v1/tickets
     def create
-      ticket = TicketProcessor.call(ticket_params)
+      Ticket.transaction do
+        return error_response('cannot book more than 10 tickets at once') if too_many_tickets?
 
-      if ticket.save
-        success_response(ticket, 201)
-      else
-        error_response(ticket.errors.messages)
+        ticket = TicketProcessor.call(ticket_params)
+
+        if ticket
+          success_response(data: ticket, status: 201, meta: created_meta(ticket))
+        else
+          error_response(ticket.errors.messages)
+        end
       end
     end
 
@@ -16,6 +21,14 @@ module Api::V1
 
     def ticket_params
       params.permit(:show_time_id, :user_id, :tickets)
+    end
+
+    def too_many_tickets?
+      ticket_params[:tickets].to_i > 10
+    end
+
+    def created_meta(ticket)
+      { ticket_count: ticket.is_a?(Array) ? ticket.count : 1 }
     end
   end
 end
