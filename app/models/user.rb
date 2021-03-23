a1040adf7cbdcae34302bbd345ebd1e7c6de8861
Jeremy Purser <jeremy@roundtriphealth.com>
2021-03-23 09:22:45 -0400

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :tickets, dependent: :destroy
+  has_many :show_times, through: :tickets
+
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 end
